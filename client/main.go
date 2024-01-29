1f61eb97b348b8a537de2b2d93e4d4c7fcda65b9
eak netpanya <eak.netpanya@gmail.com>
2024-01-30 03:52:52 +0700

    [update] client get version

--- client/main.go
+++ client/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Client:")
+	requestUrl := "https://github.com/ilmsg/sw-update/raw/main/version.txt"
+	resp, err := http.Get(requestUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", respBody)
+	// fmt.Printf("client status code: %d\n", resp.StatusCode)
 }
